14e934982e43770e49b6bce681786301046e575f
Ibara <arsip.ibara@gmail.com>
2019-05-15 07:29:24 +0700

    Update Structure Mining v1.py

--- Structure Mining v1.py
+++ Structure Mining v1.py
@@ -2,12 +2,63 @@
 import requests
 from bs4 import BeautifulSoup
 
-# For Graoh purpose
+# For Graph purpose
 import networkx as nx
 import matplotlib.pyplot as plt
 
+def simplifiedURL(url):
+    '''
+    asumsi: alamat url tidak mengandung http(s) (misalnya "true-http-website.com"
+            atau www (misalnya "true-www-website.com")
+    '''
+    # cek 1 : www
+    if "www." in url:
+        ind = url.index("www.")+4
+        url = url[ind:]
+    # cek 2 : http/https
+    if not "http" in url:
+        url = "http://"+url
+    # cek 3 : tanda / di akhir
+    if url[-1] == "/":
+        url = url[:-1]
+    return url
+
+def crawl(url, max_deep,  show=False, deep=0):
+    # returnnya ada di edgelist, 
+    global edgelist
+    
+    # menyamakan format url, agar tidak ada url yg dobel
+    url = simplifiedURL(url)
+    
+    # crawl semua link
+    links = getAllLinks(url)
+    
+    # menambah counter kedalaman
+    deep += 1
+
+    #menampilkan proses
+    if show:
+        if deep == 1:
+            print("(%d)%s" %(len(links),url))
+        else:
+            print("|", end="")
+            for i in range(deep-1): print("--", end="")
+            print("(%d)%s" %(len(links),url))
+    
+    for link in links:
+        # Membentuk format jalan (edge => (dari, ke))
+        edge = (url,link)
+        # Mengecek jalan, apabila belum dicatat, maka dimasukkan ke list
+        if not edge in edgelist:
+            edgelist.append(edge)
+        # Cek kedalaman, jika belum sampai terakhir, maka crawling.
+        if (deep != max_deep):
+            crawl(link, max_deep, show, deep)
+			
 def getAllLinks(src):
+    # Pencegahan eror apabila link yang diambil mati
     try:
+        # Get page html
         ind = src.find(':')+3
         url = src[ind:]
         page = requests.get(src)
@@ -15,11 +66,14 @@ def getAllLinks(src):
         # Mengubah html ke object beautiful soup
         soup = BeautifulSoup(page.content, 'html.parser')
 
+        # GET all tag <a>
         tags = soup.findAll("a")
 
         links = []
         for tag in tags:
+            # Pencegahan eror apabila link tidak memiliki href
             try:
+                # Get all link
                 link = tag['href']
                 if not link in links and 'http' in link:
                     links.append(link)
@@ -27,107 +81,48 @@ def getAllLinks(src):
                 pass
         return links
     except:
-        print("Error 404 : Page "+src+" not found")
+        #print("Error 404 : Page "+src+" not found")
         return list()
-    
 
-def add_nodes_from_list(old_nodes, new_nodes):
-    for node in new_nodes:
-        if not node in old_nodes:
-            old_nodes.append(node)
-    return old_nodes
-
-def add_edges_from_list(old_edges, from_, to_list):
-    for to_ in to_list:
-        edge = (from_, to)
-        if not edge in old_edges:
-            old_edges.append(edge)
-    return old_edges
-
+# Inisialisasi variabel awal
 #root = "https://www.trunojoyo.ac.id/"
 root = "http://garuda.ristekdikti.go.id/"
 nodelist = [root]
 done = [root]
 edgelist = []
+#crawl
+tampilkan = True
+crawl(root, 3, show=tampilkan)
+if tampilkan:
+    for i in range(10): print("")
 
-deep1 = getAllLinks(root)
-#nodelist = add_nodes_from_list(nodelist, deep1)
-print("banyak root : ", len(deep1))
-c=1
-for link in deep1:
-    # add edge each time accessing new link
-    edge = (root, link)
-    if not edge in edgelist:
-        edgelist.append(edge)
-    
-    if not link in done:
-        nodelist.append(link)
-        done.append(link)
-        # deep 2
-        deep2 = getAllLinks(link)
-        #print(c, link, len(deep2)); c+=1
-        
-        for link2 in deep2:
-            edge = (link, link2)
-            if not edge in edgelist:
-                edgelist.append(edge)
-            if not link2 in done:
-                nodelist.append(link2)
-                done.append(link2)
-            
-                deep3 = getAllLinks(link2)
-
-                for link3 in deep3:
-                    edge = (link2, link3)
-                    if not edge in edgelist:
-                        edgelist.append(edge)
-                    if not link3 in nodelist:
-                        nodelist.append(link3)
-
-pesan = """
-            Pilih 1 untuk Graph berarah
-            Pilih 2 untuk Graph tidak berarah
-            Selain pilihan diatas, otomatis graph tidak berarah
-        """        
-tipe_grap = input(pesan)
+#membuat Graph
 g = nx.Graph()
-if tipe_grap == "1":
-    g = g.to_directed()
+#mengubah graph menjadi graph berarah
+g = g.to_directed()
 
 # Masukin ke Graph
 g.add_edges_from(edgelist)
 
 # deklarasi pos (koordinat) (otomatis)
-pos = nx.circular_layout(g)
-pos2 = nx.random_layout(g)
-pos3 = nx.spring_layout(g)
+pos = nx.spring_layout(g)
 
 # hitung pagerank
 pr = nx.pagerank(g)
 
-# Membuat Label
+# Membuat Label && print pagerank
 print("keterangan node:")
+nodelist = g.nodes
 label= {}
-for i in range(len(nodelist)):
-    label[nodelist[i]]=i
-    print(i, nodelist[i], pr[nodelist[i]])
+for i, key in enumerate(nodelist):
+    label[key]=i
+    print(i, key, pr[key])
 
 # Draw Graph
-plt.figure(1)
+#plt.figure(1)
 plt.title('circle_layout')
 nx.draw(g, pos)
-nx.draw_networkx_labels(g, pos, label, font_size=8)
-
-plt.figure(2)
-plt.title('random_layout')
-nx.draw(g, pos2)
-nx.draw_networkx_labels(g, pos2, label, font_size=8)
-
-plt.figure(3)
-plt.title('spring_layout')
-nx.draw(g, pos3)
-nx.draw_networkx_labels(g, pos3, label, font_size=8)
-
+nx.draw_networkx_labels(g, pos, label)
 
 # show figure
 plt.axis("off")
